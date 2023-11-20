dad49cb218344d0838528e5209487b6b68d873a2
JC desktop <jackyckl1927@gmail.com>
2023-11-20 14:53:26 +0800

    tidy indentation and added log for connection success

--- school_system.js
+++ school_system.js
@@ -7,107 +7,99 @@ const bodyParser = require('body-parser');
 const secret = 'qwertyasdfgh852';
 
 const db = {
-  host: 'schoolsystem.c7floyrdntbn.ap-southeast-1.rds.amazonaws.com',
-  database: 'school_system',
-  user: 'useradmin',
-  password: '53460601',
-  port: 5432,
-  ssl: { ca: fs.readFileSync('./ap-southeast-1-bundle.pem').toString() }
+	host: 'schoolsystem.c7floyrdntbn.ap-southeast-1.rds.amazonaws.com',
+	database: 'school_system',
+	user: 'useradmin',
+	password: '53460601',
+	port: 5432,
+	ssl: { ca: fs.readFileSync('./ap-southeast-1-bundle.pem').toString() }
 };
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  if (req.path === '/login') {
-    return next();
-  }
+	if (req.path === '/login') {
+		return next();
+	}
 
-  const token = req.body.token; 
-  if (token) {
-    jwt.verify(token, secret, (err, user) => {
-      if (err) {
-        return res.json({ error: 'invalid token' });
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    res.json({ error: 'no token' });
-  }
+	const token = req.body.token; 
+	if (token) {
+		jwt.verify(token, secret, (err, user) => {
+			if (err) {
+				return res.json({ error: 'invalid token' });
+			}
+			req.user = user;
+			next();
+		});
+	} else {
+		res.json({ error: 'no token' });
+	}
 });
 
 app.use(async (req, res, next) => {
-  const client = new Client(db);
-  try {
-    await client.connect();
-    console.log(`db connected`);
-    req.dbClient = client;
-    next();
-  } catch (error) {
-    console.error('fail to connect db', error);
-  }
+	const client = new Client(db);
+	try {
+		await client.connect();
+		console.log(`db connected`);
+		req.dbClient = client;
+		next();
+	} catch (error) {
+		console.error('fail to connect db', error);
+	}
 });
 
 app.post('/test', async (req, res) => {
-  try {
-    const dbRes = await req.dbClient.query('SELECT * FROM user_login');
-    await req.dbClient.end();
-    console.log(`db disconnected`);
-    res.json(dbRes.rows);
-  } catch (error) {
-    console.error('Error on /login', error);
-  }
+	try {
+		const dbRes = await req.dbClient.query('SELECT * FROM user_login');
+		await req.dbClient.end();
+		console.log(`db disconnected`);
+		res.json(dbRes.rows);
+	} catch (error) {
+		console.error('Error on /login', error);
+	}
 });
 
 app.post('/login', async (req, res) => {
-  const { userid, password } = req.body;
+	const { userid, password } = req.body;
 
-  try {
-    let query = 'SELECT userid, password, type FROM user_login WHERE userid = $1';
-    let dbRes = await req.dbClient.query(query, [userid]);
-
+	try {
+		const query = 'SELECT userid, type FROM user_login WHERE userid = $1 AND password = $2';
+		const dbRes = await req.dbClient.query(query, [userid, password]);
+    
     if (dbRes.rows.length > 0) {
-      const user = dbRes.rows[0];
-
-      if(user.password !== password){
-        console.log('Incorrect password for userid:', userid);
-        return res.json({ error: 'authentication fail' });
-      }
-
-      const tokenPayload = { user_id: user.userid, type: user.type };
-      const token = jwt.sign(tokenPayload, secret, { expiresIn: '1h' });
-
-      res.json({ token });
-      
+		const user = dbRes.rows[0];
+		const tokenPayload = { user_id: user.userid, type: user.type };
+		const token = jwt.sign(tokenPayload, secret, { expiresIn: '1h' });
+		console.log(`autheication success`);
+		res.json({ token });
     } else {
-      console.log('Userid not found:', userid);
-      res.json({ error: 'authentication fail' });
+		res.json({ error: 'authentication fail' });
     }
     
   } catch (error) {
-    console.error('Error on /login', error);
-    res.status(500).json({ message: 'db connection fail' });
+		console.error('Error on /login', error);
+		res.status(500).json({ message: 'db connection fail' });
   } finally {
-    await req.dbClient.end();
-    console.log(`db disconnected`);
+		await req.dbClient.end();
+		console.log(`db disconnected`);
   }
 });
 
 app.post('/student_info', async (req, res) => {
-  const { user_id } = req.body;
-  try {
-    const query = 'SELECT * FROM students WHERE student_userid = $1';
-    const dbRes = await req.dbClient.query(query, [user_id]);
-    await req.dbClient.end();
-    console.log(`db disconnected`);
-    res.json(dbRes.rows);
-  } catch (error) {
-    console.error('Error', error);
-    res.status(500).json({ message: 'Error fetching student info' });
-  }
+	const { user_id } = req.body;
+	try {
+		const query = 'SELECT * FROM students WHERE student_userid = $1';
+		const dbRes = await req.dbClient.query(query, [user_id]);
+		await req.dbClient.end();
+		console.log(`db disconnected`);
+		res.json(dbRes.rows);
+	} catch (error) {
+		console.error('Error', error);
+		res.status(500).json({ message: 'Error fetching student info' });
+	}
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+	console.log(`Server is running on port ${PORT}.`);
 });
